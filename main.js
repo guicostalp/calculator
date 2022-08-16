f71ee871553876c279045742e5d4931a394fa7ec
Gui Costa <gui.costalp@gmail.com>
2022-08-16 20:20:02 +1200

    Buttons add. Working on sum function

--- main.js
+++ main.js
@@ -1,41 +1,101 @@
 number1 = 0;
 number2 = 0;
+operation = ""
 
-function reset(){
+function start () {
+    displayButtons();
+    getOperation();
 
-    document.querySelector(".display").textContent = "asd";
-    number1 = 0;
-    number2 = 0;
 
 }
 
-let buttons = document.querySelectorAll('button')
-    buttons.forEach((button) => 
-        button.addEventListener("click", () => {
+function getOperation (){
 
-            
-            const text = button.textContent
-            document.querySelector(".display").textContent = `${document.querySelector(".display").textContent}${text}`
+    let sumButton = document.querySelector('.sum')
+        sumButton.addEventListener("click", () => {
+        
+        number1 = document.querySelector(".display").textContent;
+        document.querySelector(".display").textContent = "";
+
+        operation = "sum"
+
+    }) 
+
+    let subtractButton = document.querySelector('.subtract')
+        subtractButton.addEventListener("click", () => {
             
             number1 = document.querySelector(".display").textContent;
-            console.log(number1);
+            document.querySelector(".display").textContent = "";
+
+            operation = "subtract"
+
+        }) 
+
+    let multiplyButton = document.querySelector('.multiply')
+        multiplyButton.addEventListener("click", () => {
+            
+            number1 = document.querySelector(".display").textContent;
+            document.querySelector(".display").textContent = "";
+
+            operation = "multiply"
+
         })
-    )
+        
+    let divideButton = document.querySelector('.divide')
+        divideButton.addEventListener("click", () => {
+            
+            number1 = document.querySelector(".display").textContent;
+            document.querySelector(".display").textContent = "";
+
+            operation = "divide"
+
+        }) 
+
+}
+
+function displayButtons() {
+    let buttons = document.querySelectorAll('.number')
+        buttons.forEach((button) => 
+            button.addEventListener("click", () => {
+                
+                const text = button.textContent
+                document.querySelector(".display").textContent = 
+                    `${document.querySelector(".display").textContent}${text}`
+                
+            })
+        )
+
+
+}
+
+function reset(){
+    document.querySelector(".display").textContent = "";
+    number1 = 0;
+    number2 = 0;
+}
 
 
 function operate (number1, number2, operation) {
-    if (operation === "+") {
-        return sum(number1,number2);
 
-    } else if(operation === "-") {
+    number2 = document.querySelector(".display").textContent;
 
-        return subtract(number1, number2);
-    } else if(operation === "*") {
 
-        return multiply(number1, number2);
+    if (operation === "sum") {
+        document.querySelector(".display").textContent = 
+            `${sum(number1,number2)}`;
+
+    } else if(operation === "subtract") {
+        document.querySelector(".display").textContent = 
+            `${subtract(number1,number2)}`;
+
+    } else if(operation === "multiply") {
+        document.querySelector(".display").textContent = 
+            `${multiply(number1,number2)}`;
+
     } else {
 
-        return divide(number1, number2);
+        document.querySelector(".display").textContent = 
+            `${divide(number1,number2)}`;
     }
 
 }
@@ -45,13 +105,18 @@ function operate (number1, number2, operation) {
 function sum(number1, number2) {
     const newNumber = number1 + number2
 
+    console.log(newNumber);    
+
     return newNumber;
 
 }
 
+
+
+
 function subtract(number1, number2) {
     const newNumber = number1 - number2
-
+    console.log(newNumber);
     return newNumber;
 
 }
@@ -69,3 +134,5 @@ function divide(number1, number2) {
     return newNumber;
 
 }
+
+start();
